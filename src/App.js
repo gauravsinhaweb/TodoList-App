c0c922fd920fd7568b7542597544aa91f73f3aa6
gauravsinha18 <59147380+gauravsinha18@users.noreply.github.com>
2021-01-19 01:23:23 +0530

    'update'

--- src/App.js
+++ src/App.js
@@ -1,85 +1,12 @@
+import React from "react";
+import Hero from "./Components/Hero";
 import "./App.css";
-import Todo from "./Todo";
-import React, { useState, useEffect } from "react";
-import db from "./firebase";
-import firebase from "firebase";
-import { TextField, Button } from "@material-ui/core";
-
-// const styles = makeStyles((theme) => ({
-//   container: {
-//     width: 530,
-//     [theme.breakpoints.down("xs")]: {
-//       width: 100,
-//     },
-//   },
-// }));
-
-export default function App() {
-  const [todos, setTodos] = useState([""]);
-  const [input, setInput] = useState("");
-  const classes = useState("");
-  useEffect(() => {
-    db.collection("todos")
-      .orderBy("timestamp", "desc")
-      .onSnapshot((snapshot) => {
-        setTodos(
-          snapshot.docs.map((doc) => ({ id: doc.id, todo: doc.data().todo }))
-        );
-      });
-  }, []);
-
-  const addTodo = (event) => {
-    event.preventDefault(); //stop refreshing page ..store todos IMPORTANT!
-    db.collection("todos").add({
-      todo: input,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-    });
-    // setTodos([...todos, input])
-    setInput("");
-  };
-
+function App() {
   return (
-    <div className="container">
-      <h1 className="heading">Todo-List</h1>
-      <form className="form">
-        <TextField
-          InputLabelProps={{
-            style: { color: "#fff" },
-          }}
-          className="textfield"
-          value={input}
-          onChange={(event) => setInput(event.target.value)}
-          id="standard-basic"
-          label="New Task.."
-        />
-
-        <Button
-          variant="contained"
-          size="large"
-          className={classes.margin}
-          color="secondary"
-          disabled={!input}
-          type="submit"
-          onClick={addTodo}
-        >
-          ADD{" "}
-        </Button>
-
-        {/* <AddCircleIcon
-          disabled={!input}
-          type="submit"
-          onClick={addTodo}
-          variant="contained"
-          color="secondary"
-        >
-          Add todos
-        </AddCircleIcon> */}
-      </form>
-      <ul>
-        {todos.map((todo, i) => (
-          <Todo todo={todo} />
-        ))}
-      </ul>
+    <div class="bg-dark">
+      <Hero />
     </div>
   );
 }
+
+export default App;
--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import db from "../firebase";
-import "./TodoList.css";
-
+import { ListItemText, ButtonGroup, Button } from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
+import "./Todo.css";
 function Todo(props) {
   const [state, setState] = useState(false);
   const [edit, setEdit] = useState("");
   const [save, setSave] = useState("false");
+  const classes = useState("");
 
   const editHandler = () => {
     db.collection("todos").doc(props.todo.id).set(
@@ -17,35 +20,49 @@ function Todo(props) {
     setState(save);
   };
   return (
-    <div className="list">
-      <li>
-        {state ? (
-          <form>
-            <input
-              placeholder={props.todo.todo}
-              value={edit}
-              onChange={(event) => setEdit(event.target.value)}
-            />
-            <button disabled={!edit} onClick={(e) => setSave(editHandler)}>
-              save
-            </button>
-          </form>
-        ) : (
-          props.todo.todo
-        )}
-      </li>
+    <div>
       <div>
-        <button
-          onClick={(event) =>
-            db.collection("todos").doc(props.todo.id).delete()
-          }
-        >
-          delete
-        </button>
-
-        <button onClick={setState}>edit</button>
+        <ListItemText className="list">
+          {state ? (
+            <form className={classes.root}>
+              <input
+                placeholder={props.todo.todo}
+                value={edit}
+                onChange={(event) => setEdit(event.target.value)}
+                label="Filled"
+                variant="filled"
+              />{" "}
+              <Button
+                variant="contained"
+                size="small"
+                color="primary"
+                disabled={!edit}
+                type="submit"
+                onClick={(e) => setSave(editHandler)}
+              >
+                Save
+              </Button>
+            </form>
+          ) : (
+            props.todo.todo
+          )}
+        </ListItemText>
+        <ButtonGroup disableElevation variant="contained" color="primary">
+          <DeleteIcon
+            onClick={(event) =>
+              db.collection("todos").doc(props.todo.id).delete()
+            }
+            variant="outlined"
+            color="secondary"
+          />
+          <EditIcon onClick={setState} />
+        </ButtonGroup>
+        <hr className="hr" />
       </div>
-      <hr />
+      <div className="footer">
+        This Application is connected to google firebase. So the Database will
+        gonna expire anytime sooner. Its's build for test purpose.
+      </div>
     </div>
   );
 }
